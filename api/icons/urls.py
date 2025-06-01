97e7fd6ae8c3accabb46998d698eed6d83cb9812
Yuhesh Pandian P J <yuheshpandian@gmail.com>
2025-06-02 02:35:07 +0530

    Fixed the bug that caused internal error

--- api/icons/urls.py
+++ api/icons/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import get_icon, index_page, icon_gallery_view, server_error
+from . import views
 
 urlpatterns = [
-    path("<str:theme>/<str:icon_name>", get_icon),
-    path("", index_page),
-    path("gallery/", icon_gallery_view, name="icon-gallery"),
-    # path("500/", server_error, name="error_500"),
+    path("<str:theme>/<str:icon_name>", views.get_icon),
+    path("", views.index_page),
+    path("gallery/", views.icon_gallery_view, name="icon-gallery"),
+    # path("500/", views.server_error, name="error_500"),
 ]
